a890ff0beb836cdb73d72986989cca0304c04056
Eric Wood <eric@ericwood.org>
2017-02-11 17:23:28 -0500

    Early version of minilogue status bar

--- webpack/application.jsx
+++ webpack/application.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PatchList from './components/patch-list';
 import MidiWarning from './components/midi-warning';
+import MinilogueStatus from './components/minilogue-status';
 import oscilloscope from './oscilloscope';
 
 function ready(fn) {
@@ -16,5 +17,5 @@ ready(() => {
   oscilloscope();
   ReactDOM.render(<PatchList />, document.querySelector('#patch-list'));
   ReactDOM.render(<MidiWarning />, document.querySelector('#midi-info'));
-  // ReactDOM.render(<MinilogueStatus />, document.querySelector('#minilogue-status'));
+  ReactDOM.render(<MinilogueStatus />, document.querySelector('#minilogue-status'));
 });
--- webpack/components/minilogue-status.jsx
+++ webpack/components/minilogue-status.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import withMidi, { propTypes as midiPropTypes } from './with-midi';
 
-const MinilogueStatus = () => {
+const MinilogueStatus = (props) => {
+  let status;
+
+  switch (props.midi.status) {
+    case 'available':
+      status = props.midi.input ? 'connected' : 'disconnected';
+      break;
+    case 'unavailable':
+      status = 'disconnected (web midi not supported)'; // TODO: elaborate
+      break;
+    case 'declined':
+      status = 'disconnected (web midi sysex not allowed)';
+      break;
+    case 'pending':
+      status = 'connecting...';
+      break;
+    default:
+      status = '';
+  }
+
   return (
-    <div />
+    <div>
+      Minilogue: {status}
+    </div>
   );
 };
 
